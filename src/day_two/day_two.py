a3d46f5368b9ad109a13d46f8d97ebfd2465f35a
Tate Anderson <tate.anderson@huskers.unl.edu>
2023-12-09 12:04:30 -0600

    day 2 complete!

--- src/day_two/day_two.py
+++ src/day_two/day_two.py
@@ -2,7 +2,6 @@ def parse( line: str ) -> int:
     firstSplit = line.split(':')
     value = int(firstSplit[0].replace("Game ", ""))
     secondSplit = firstSplit[1].split(';')
-    print(secondSplit)
 
     for draw in secondSplit:
         colors = draw.split(',')
@@ -28,6 +27,34 @@ def validate_draw( colors : list ) -> bool:
                 return False
     return True
 
+def find_min_possible( colors : list ) -> list:
+    vals = {"red" : 0, "blue" : 0, "green" : 0}
+    for color in colors:
+        if "red" in color:
+            val = int(color.replace(" red", ""))
+            vals["red"] = val
+        if "green" in color:
+            val = int(color.replace(" green", ""))
+            vals["green"] = val
+        if "blue" in color:
+            val = int(color.replace(" blue", ""))
+            vals["blue"] = val
+    return vals
+
+def find_power( line: str ) -> int:
+    color_strs = ["red", "blue", "green"]
+    firstSplit = line.split(':')
+    secondSplit = firstSplit[1].split(';')
+    min_vals = {"red" : -1, "green" : -1, "blue" : -1}
+
+    for draw in secondSplit:
+        colors = draw.split(',')
+        vals = find_min_possible( colors )
+        for c in color_strs:
+            min_vals[c] = max(min_vals[c], vals[c])
+    print(min_vals)
+
+    return min_vals["red"] * min_vals["green"] * min_vals["blue"]
 
 def day_two():
     input = open('src/day_two/input2.txt', 'r')
@@ -35,7 +62,7 @@ def day_two():
     res = 0
 
     for line in lines:
-        res += parse(line)
+        res += find_power(line)
 
     return res
 
