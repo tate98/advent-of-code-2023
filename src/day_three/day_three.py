e02a7c1ec07430fb7830235781d8dc7294430097
Tate Anderson <tate.anderson@huskers.unl.edu>
2023-12-15 17:36:29 -0600

    day 3 - part 2

--- src/day_three/day_three.py
+++ src/day_three/day_three.py
@@ -1,6 +1,7 @@
 import numpy as np
+import collections
 
-def day_three() -> int:
+def day_three_p1() -> int:
     input = np.loadtxt('src/day_three/input3.txt', dtype=str, comments=None)
     numbers = "0123456789"
     cur_num = ""
@@ -43,8 +44,69 @@ def check_num_good(i, j, input) -> bool:
                     return True
     return False
 
+def day_three_p2():
+    d = collections.defaultdict(list)
+    input = np.loadtxt('src/day_three/input3.txt', dtype=str, comments=None)
+    numbers = "0123456789"
+    res = 0
+
+    i, j = 0, 0
+
+    while i < len(input):
+        while j < len(input[0]):
+            if input[i][j] in numbers:
+                num = get_num(i, j, input, numbers)
+                numLen = len(num)
+                gear = find_gear(i, j, numLen, input)
+                if gear:
+                    d[gear].append(int(num))
+                j += numLen
+            else:
+                j += 1
+        j = 0
+        i += 1
+
+    for key in d:
+        if len(d[key]) > 1:
+            res += d[key][0] * d[key][1]
+
+    return res
+
+
+def get_num(row, col, input, numbers) -> str:
+    num = input[row][col]
+    if col < len(input[0]) - 1 and input[row][col + 1] in numbers:
+        num += input[row][col + 1]
+    if col < len(input[0]) - 2 and input[row][col + 2] in numbers:
+        num += input[row][col + 2]
+
+    return num
+
+def find_gear(row, col, numLen, input):
+    if numLen == 1:
+        dirsi = [1, 0, -1]
+        dirsj = [1, 0, -1]
+    
+    elif numLen == 2:
+        dirsi = [1, 0, -1]
+        dirsj = [-1, 0, 1, 2]
+
+    elif numLen == 3:
+        dirsi = [1, 0, -1]
+        dirsj = [-1, 0, 1, 2, 3]
+
+    for i in dirsi:
+        for j in dirsj:
+            di = row + i
+            dj = col + j
+            if di >= 0 and di < len(input) and dj >= 0 and dj < len(input[0]) and input[di][dj] == '*':
+                    return (di, dj)
+            
+    return None
+
+
 def main():
-    print(day_three())
+    print(day_three_p2())
 
 if __name__ == "__main__":
     main()
